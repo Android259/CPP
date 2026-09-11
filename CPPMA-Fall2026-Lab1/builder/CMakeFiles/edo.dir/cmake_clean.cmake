141eff482bb9979406ae9afe77f35d212bced4b1
Kalinin <andrei.kalinin@grenoble-inp.org>
2026-09-11 09:02:06 +0200

    first commit

---
+++ CPPMA-Fall2026-Lab1/builder/CMakeFiles/edo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/edo.dir/link.d"
+  "CMakeFiles/edo.dir/edo.cxx.o"
+  "CMakeFiles/edo.dir/edo.cxx.o.d"
+  "edo"
+  "edo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/edo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
